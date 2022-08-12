cec5405cd965cbceb3e4bd42cf651ae539aff1bd
Antti Holvikari <antti@anttih.com>
2022-08-12 16:29:52 +0300

    Allow unfree

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,8 @@
     '';
   };
 
+  nixpkgs.config.allowUnfree = true;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
