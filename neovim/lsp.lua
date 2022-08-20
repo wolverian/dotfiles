45b0e0708994b2b45ba6bd7202f4b874a1242d23
Antti Holvikari <antti@anttih.com>
2022-08-20 23:21:14 +0300

    neovim: purescript lsp

--- neovim/lsp.lua
+++ neovim/lsp.lua
@@ -33,6 +33,10 @@ lspconfig.hls.setup {
   on_attach = on_attach
 }
 
+lspconfig.purescriptls.setup {
+  on_attach = on_attach
+}
+
 lspconfig.sumneko_lua.setup {
   on_attach = on_attach,
   settings = {
